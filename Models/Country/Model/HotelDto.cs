b7c764805d8128a708be9a17ba7ca5b90ab3f0cf
rushi-smb <rushi@smbios.co.in>
2023-02-09 00:19:49 +0530

    Refactor Get Method

---
+++ Models/Country/Model/HotelDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelListing.API.Models.Country.Model
+{
+    public class HotelDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Rating { get; set; }
+        public int CountryId { get; set; }
+    }
+}
