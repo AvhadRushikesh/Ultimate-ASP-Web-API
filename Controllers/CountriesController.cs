a03b374eb94351a0f131e091e4ce7831cebafa4c
rushi-smb <rushi@smbios.co.in>
2023-02-08 23:26:43 +0530

    AutoMapper

--- Controllers/CountriesController.cs
+++ Controllers/CountriesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using HotelListing.API.Data;
 using HotelListing.API.Models.Country;
+using AutoMapper;
 
 namespace HotelListing.API.Controllers
 {
@@ -15,10 +16,12 @@ namespace HotelListing.API.Controllers
     public class CountriesController : ControllerBase
     {
         private readonly HotelListingDbContext _context;
+        private readonly IMapper _mapper;
 
-        public CountriesController(HotelListingDbContext context)
+        public CountriesController(HotelListingDbContext context, IMapper mapper)
         {
             _context = context;
+            this._mapper = mapper;
         }
 
         // GET: api/Countries
@@ -86,19 +89,9 @@ namespace HotelListing.API.Controllers
         [HttpPost]
         public async Task<ActionResult<Country>> PostCountry(CreateCountryDto createCountry)
         {
-            var country = new Country
-            {
-                Name = createCountry.Name,
-                ShortName = createCountry.ShortName,
-            };
-
-          if (_context.Countries == null)
-          {
-              return Problem("Entity set 'HotelListingDbContext.Countries'  is null.");
-          }
+           var country= _mapper.Map<Country>(createCountry);          
             _context.Countries.Add(country);
             await _context.SaveChangesAsync();
-
             return CreatedAtAction("GetCountry", new { id = country.Id }, country);
         }
 
