1b8e71ac38ce69d06e5b77ebf27a22d36c7524bd
rushi-smb <rushi@smbios.co.in>
2023-02-09 18:33:03 +0530

    Commit upto Section 7

---
+++ Models/Country/Hotel/BaseHotelDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.API.Models.Country.Model
+{
+    public abstract class BaseHotelDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        public double? Rating { get; set; }  //If nothing provice by default it will be zero so with question mark it becames Nullable
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int CountryId { get; set; }
+    }
+}
---
+++ Models/Country/Hotel/CreateHotelDto.cs
@@ -0,0 +1,7 @@
+﻿namespace HotelListing.API.Models.Country.Model
+{
+    public class CreateHotelDto : BaseHotelDto
+    {
+
+    }
+}
